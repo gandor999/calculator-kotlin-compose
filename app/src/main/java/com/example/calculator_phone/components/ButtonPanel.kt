4432fe8f3d810d7ec78ec3a7b7c3e778e0f733b1
Geodor Ruales <gandor999@gmail.com>
2023-10-28 20:33:26 +0800

    feat: make Button Panel

---
+++ app/src/main/java/com/example/calculator_phone/components/ButtonPanel.kt
@@ -0,0 +1,95 @@
+package com.example.calculator_phone.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.example.calculator_phone.state_keeping.CalculatorStates
+import com.example.calculator_phone.util.calculate
+
+@Composable
+fun ButtonPanel(
+    modifier: Modifier = Modifier,
+    calculatorStateKeeper: CalculatorStates,
+) {
+    val orientation = calculatorStateKeeper.config.orientation
+
+    Column(
+        modifier = modifier
+    ) {
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            val buttonIndexes = listOf(listOf(0, 7), listOf(8, 15))
+
+            for (buttonIndex in buttonIndexes) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    RenderButtons(
+                        orientation = orientation,
+                        calculatorStateKeeper = calculatorStateKeeper,
+                        fromIndex = buttonIndex[0],
+                        toIndex = buttonIndex[1]
+                    )
+                }
+            }
+        } else {
+            val buttonIndexes = listOf(listOf(0, 3), listOf(4, 7), listOf(8, 11), listOf(12, 15))
+
+            for (buttonIndex in buttonIndexes) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    RenderButtons(
+                        orientation = orientation,
+                        calculatorStateKeeper = calculatorStateKeeper,
+                        fromIndex = buttonIndex[0],
+                        toIndex = buttonIndex[1]
+                    )
+                }
+            }
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+        ) { // these are invisible buttons so that we can align the buttons perfectly either landscape or orientation
+            for (i in 0 until (if (orientation == Configuration.ORIENTATION_LANDSCAPE) 6 else 2)) {
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(Color.Transparent),
+                    elevation = ButtonDefaults.elevation(0.dp)
+                ) {}
+            }
+
+            Button(onClick = {
+                calculatorStateKeeper.setInputNumber(
+                    TextFieldValue(
+                        calculatorStateKeeper.inputNumber.text.dropLast(
+                            1
+                        )
+                    )
+                )
+            }, colors = ButtonDefaults.buttonColors(Color(red = 146, green = 55, blue = 77))) {
+                Text("<")
+            }
+
+            Button(onClick = {
+                calculatorStateKeeper.setInputNumber(TextFieldValue(calculate(calculatorStateKeeper.inputNumber.text).toString()))
+            }, colors = ButtonDefaults.buttonColors(Color(red = 81, green = 187, blue = 254))) {
+                Text("=")
+            }
+        }
+    }
+}
