d40911b796601797da62ab7658519820e55b7b89
Geodor Ruales <gandor999@gmail.com>
2023-10-30 17:02:11 +0800

    refactor: will not need to show error on pop up dialog

--- app/src/main/java/com/example/calculator_phone/state_keeping/stateKeepers.kt
+++ app/src/main/java/com/example/calculator_phone/state_keeping/stateKeepers.kt
@@ -2,7 +2,6 @@ package com.example.calculator_phone.state_keeping
 
 import android.content.res.Configuration
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.focus.FocusManager
@@ -13,7 +12,6 @@ import androidx.compose.ui.text.input.TextFieldValue
 data class CalculatorStates(
     val inputNumber: TextFieldValue,
     val setInputNumber: (TextFieldValue) -> Unit,
-    val openErrorDialog: MutableState<Boolean>,
     val focusManager: FocusManager,
     val config: Configuration
 )
@@ -21,8 +19,7 @@ data class CalculatorStates(
 @Composable
 fun initCalculatorStates(): CalculatorStates {
     val (inputNumber, setInputNumber) = remember { mutableStateOf(TextFieldValue("")) }
-    val openErrorDialog = remember { mutableStateOf(false) }
     val focusManager = LocalFocusManager.current
     val config = LocalConfiguration.current
-    return CalculatorStates(inputNumber, setInputNumber, openErrorDialog, focusManager, config)
+    return CalculatorStates(inputNumber, setInputNumber, focusManager, config)
 }
